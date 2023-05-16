a574493ec196661a2671479df8cf0c001d35ea72
Yjason-K <81736873+Yjason-K@users.noreply.github.com>
2023-05-16 22:28:16 +0900

    [Silver III] Title: 다음 순열, Time: 60 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+num = list(map(int, input().split()))
+
+# 뒤에서 부터 값을 조회하며 i이 i-1보다 큰 경우 찾기(target)
+for i in range(n-1, 0, -1):
+    if num[i-1] < num[i]:
+        target = i-1
+        for j in range(n-1, 0, -1):
+            # 다시 뒤부터 순회하며 i-1일 때 보다 큰 값은 위치 바꾸어 주기
+            if num[target] < num[j]:
+                num[target], num[j] = num[j], num[target]
+                # target을 기준으로 배열을 나누고 뒤 부분은 새로 정렬하기
+                answer = num[:target+1] + sorted(num[target+1:])
+                print(*answer)
+                exit() # 정답을 찾았기 때문에 코드 종료
+
+# 그외의 경우 -1 출력
+print('-1')
