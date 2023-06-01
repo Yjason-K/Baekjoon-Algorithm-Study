17142fecc3f67ef723188ba57cd94cdde827cb8a
Yjason-K <81736873+Yjason-K@users.noreply.github.com>
2023-06-01 12:17:37 +0900

    [Silver I] Title: 미로 탐색, Time: 168 ms, Memory: 116740 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,27 +1,27 @@
-def bfs(si, sj, ei, ej):
-    q = []
-    v = [[0]*M for _ in range(N)]
-
-    q.append((si,sj))
-    v[si][sj] = 1
-
-    while q:
-        ci, cj = q.pop(0)
-        if (ci,cj) == (ei,ej):  # 정답 처리가 필요한 경우 이자리에서..
-            return v[ei][ej]
-
-        # 4방향, 범위내, 조건에 맞으면: arr == 1, v == 0
-        for di,dj in ((-1,0), (1,0), (0,-1), (0,1)):
-            ni, nj = ci+di, cj+dj
-            if 0<=ni<N and 0<=nj<M and arr[ni][nj]==1 and v[ni][nj]==0:
-                q.append((ni,nj))
-                v[ni][nj] = v[ci][cj]+1
-
+from collections import deque
 N, M = map(int, input().split())
 arr = [list(map(int, input())) for _ in range(N)]
-
-ans = bfs(0, 0, N-1, M-1)
-print(ans)
+v = [[0]*M for _ in range(N)]
 
 
+def bfs(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
+    q = deque()
+    q.append((x, y))
+    v[x][y] = 1
+
+    while q:
+        cx, cy = q.popleft()
+
+        for i in range(4):
+            nx, ny = cx + dx[i], cy + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 1 and v[nx][ny] == 0:
+                q.append((nx, ny))
+                v[nx][ny] = v[cx][cy] + 1
+
+    return v[N-1][M-1]
+
+
+print(bfs(0, 0))
