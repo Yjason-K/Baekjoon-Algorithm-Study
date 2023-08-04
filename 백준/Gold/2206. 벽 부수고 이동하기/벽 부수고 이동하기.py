4aa878118040f5565a47ac3514024124ae635fb7
Yjason-K <81736873+Yjason-K@users.noreply.github.com>
2023-08-04 23:23:33 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3964 ms, Memory: 192020 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,35 +1,40 @@
 from collections import deque
+import sys
 
+# bfs()
 def bfs():
-    dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
-    q = deque()
-    q.append([0, 0, 1])
-    visited = [[[0]*2 for _ in range(m)] for _ in range(n)]
-    visited[0][0][1] = 1
+  q = deque()
+  q.append([0, 0, 1]) # 시작위치 설정
+  # 벽을 부쉈는지에 대한 확인을 위한 3차원 방문 배열 설정
+  visited = [ [[0] * 2 for _ in range(M)] for _ in range(N) ]
+  visited[0][0][1] = 1 # (1,1) 위치 설정
+  
+  while q:
+    x, y, wall = q.popleft()
     
-    while q:
-        x, y, wall = q.popleft()
-        
-        if x == n-1 and y == m-1:
-            return visited[x][y][wall]
-        
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            
-            if 0 <= nx < n and 0 <= ny < m:
-                # If there is no wall
-                if matrix[nx][ny] == 0 and visited[nx][ny][wall] == 0:
-                    visited[nx][ny][wall] = visited[x][y][wall] + 1
-                    q.append([nx, ny, wall])
-                
-                # If there is a wall and you can break it
-                elif matrix[nx][ny] == 1 and wall == 1:
-                    visited[nx][ny][0] = visited[x][y][wall] + 1
-                    q.append([nx, ny, 0])
-    
-    return -1
+    # (2,2) 행렬인 경우
+    if x == N-1 and y == M-1:
+      return visited[x][y][wall] # 도착했을 때의 거리를 반환합니다.
 
-n, m = map(int, input().split())
-matrix = [list(map(int, list(input().strip()))) for _ in range(n)]
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+      nx, ny = x + dx, y + dy
+      
+      if 0 <= nx < N and 0 <= ny < M:
+        # 벽이 없는 경우
+        if arr[nx][ny] == 0 and visited[nx][ny][wall] == 0:
+          visited[nx][ny][wall] = visited[x][y][wall] + 1
+          q.append([nx, ny, wall])
+        
+        # 벽이 있고 부술 수 있는 경우
+        elif arr[nx][ny] == 1 and wall == 1:
+          visited[nx][ny][0] = visited[x][y][wall] + 1
+          q.append([nx, ny, 0])
+        
+  return -1
+
+# 입력값 설정
+input = sys.stdin.readline
+N, M = map(int, input().split())
+arr = [ list(map(int, list(input().strip()))) for _ in range(N) ]
 
 print(bfs())
