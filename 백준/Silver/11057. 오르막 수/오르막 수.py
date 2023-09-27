8afe89f4791b07e151fa05e9a89cda90df333bc7
Yjason-K <81736873+Yjason-K@users.noreply.github.com>
2023-09-27 20:47:43 +0900

    [Silver I] Title: 오르막 수, Time: 144 ms, Memory: 115752 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,32 +1,10 @@
-# 2차원으로 문제를 푸는 경우
-# N = int(input())
-
-# arr = [ [0] * 10 for _ in range(N+1) ]
-# arr[1] = [1] * 10
-    
-# for i in range(2, N+1):
-#     for j in range(0, 10):
-#         arr[i][j] = sum(arr[i-1][j:])
-
-
-# print(sum(arr[N])%10007)
-
-#N = int(input())
-
-#dp = [1] * 10
-
-#for i in range(2, N+1):
-#    for j in range(10):
-#        dp[j] = sum(dp[j:])
-        
-#print(sum(dp)%10007)        
-
 N = int(input())
 
-dp = [1] * 10
+arr = [ [0] * 10 for _ in range(N+1) ]
+arr[1] = [1] * 10
+    
+for i in range(2, N+1):
+    for j in range(0, 10):
+        arr[i][j] = sum(arr[i-1][j:])
 
-for _ in range(N-1):
-    for j in range(1, 10):
-        dp[j] += dp[j-1]
-        
-print(sum(dp)%10007)
+print(sum(arr[N])%10007)
