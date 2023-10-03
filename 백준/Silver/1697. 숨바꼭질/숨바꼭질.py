633723650ff5ac29ddf9255649a1557dbc24e113
Yjason-K <81736873+Yjason-K@users.noreply.github.com>
2023-10-03 22:53:17 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35344 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,24 +1,36 @@
-def bfs(S, E):
-    q = []
-    v = [0] * 2000000
+import sys
+from collections import deque
+input = sys.stdin.readline
 
-    q.append(S)
-    v[S] = 1
-
-    while q:
-        c = q.pop(0)
-
-        if c == E:
-            return v[E] - 1
-
-        for i in (c-1, c+1, c*2):
-            n = i
-            if 0<= n <= 200000 and v[n] == 0:
-                q.append(n)
-                v[n] = v[c] + 1
-    return -1  # 값이 잘못된 경우
+# bfs 탐색을 통한 동생의 위치 찾기
 
 
-S, E = map(int, input().split())
-ans = bfs(S, E)
-print(ans)
+def short_time():
+    que = deque()
+    # 순간이동의 경우 2배가 되기때문에 방문배열을 200000까지
+    visited = [0] * 100001
+
+    que.append(N)  # 수빈이 위치로 시작
+    visited[N] = 1  # 시작위치, 1초부터 시작
+
+    # bfs
+    while que:
+        cur_position = que.popleft()
+
+        # 현재 위치가 동생위치라면 그때의 방문초 return하기
+        if cur_position == K:
+            return visited[K] - 1  # 1초부터 시작 했기 때문에 - 1
+
+        for next_position in (cur_position-1, cur_position+1, cur_position*2):
+            if 0 <= next_position <= 100000 and visited[next_position] == 0:
+                que.append(next_position)
+                visited[next_position] = visited[cur_position] + 1
+
+    return -1  # 동생 위치에 도달하지 못하는 경우
+
+
+# 수빈이 위치, 동생 위치
+N, K = map(int, input().split())
+
+print(short_time())
+
