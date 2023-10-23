70697706c99dfb60448e6b4de9bc671a41554eca
Yjason-K <81736873+Yjason-K@users.noreply.github.com>
2023-10-23 21:55:34 +0900

    [level 3] Title: 네트워크, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,20 @@
-def dfs(computers, visited, start):
-    stack = [start]
-    while stack:
-        node = stack.pop()
+from collections import deque
+
+def bfs(computers, visited, start):
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
         if visited[node] == 0:
             visited[node] = 1
-            connected_nodes = [i for i, value in enumerate(computers[node]) if value == 1 and visited[i] == 0]
-            stack.extend(connected_nodes)
+            for i, value in enumerate(computers[node]):
+                if value == 1 and visited[i] == 0:
+                    queue.append(i)
 
 def solution(n, computers):
     visited = [0] * n
     answer = 0
     for i in range(n):
         if visited[i] == 0:
-            dfs(computers, visited, i)
+            bfs(computers, visited, i)
             answer += 1
     return answer
