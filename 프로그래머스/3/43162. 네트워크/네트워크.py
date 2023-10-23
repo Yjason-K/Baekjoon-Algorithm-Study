73c70d8cbdce51c3e949ac57deb04f052c0050d1
Yjason-K <81736873+Yjason-K@users.noreply.github.com>
2023-10-23 22:03:53 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -4,17 +4,23 @@ def bfs(computers, visited, start):
     queue = deque([start])
     while queue:
         node = queue.popleft()
+        
+        # 방문하지 않은 경우만 처리
         if visited[node] == 0:
+            # 방문으로 바꾸기
             visited[node] = 1
             for i, value in enumerate(computers[node]):
+                # 연결된 컴퓨터이고 아직 방문하지 않았다면
                 if value == 1 and visited[i] == 0:
                     queue.append(i)
 
 def solution(n, computers):
     visited = [0] * n
-    answer = 0
+    num_network = 0
+    
     for i in range(n):
         if visited[i] == 0:
             bfs(computers, visited, i)
-            answer += 1
-    return answer
+            num_network += 1
+            
+    return num_network
