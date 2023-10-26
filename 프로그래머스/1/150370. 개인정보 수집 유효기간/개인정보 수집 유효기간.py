6e0e0d471768c5e3efce9a1e199d488895bb2a13
Yjason-K <81736873+Yjason-K@users.noreply.github.com>
2023-10-26 13:34:24 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.23 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,3 +1,4 @@
+# 연,월,일 -> 일
 def dateToday(date):
     year, month, day = map(int, date.split("."))
     return int((year * 12 * 28) + (month * 28) + day)
@@ -6,11 +7,13 @@ def solution(today, terms, privacies):
     terms_val = dict()
     answer = []
     
+    # 약관 종류 등록
     for var in terms:
         a, b = var.split(' ')
         b = int(b)
         terms_val[a] = b
-        
+    
+    # 수집된 개인정보 확인    
     for index, val in enumerate(privacies):
         d, v = val.split(' ')
         priv_date = dateToday(d)
